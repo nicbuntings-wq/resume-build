5dad16fc3cc72e7b67961a8501a0f4f735d87b9d
nicbuntings-wq <nicbuntings@gmail.com>
2025-09-16 11:44:58 +1000

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,11 +41,12 @@ export const metadata: Metadata = {
   formatDetection: { email: false, address: false, telephone: false },
   icons: {
     icon: [
+      { url: "/favicon-48x48.png", sizes: "48x48", type: "image/png" },
       { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
       { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
       { url: "/favicon.ico", sizes: "any", type: "image/x-icon" },
     ],
-    apple: "/overwrite-apple-touch-icon.png",
+    apple: "/apple-touch-icon.png",
   },
   openGraph: {
     type: "website",
